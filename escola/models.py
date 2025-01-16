30046dea911f95d2ca7e6d92b1afd0d4b4a3175a
Samuel Treptow <pessoal.swtreptow@gmail.com>
2025-01-16 16:13:13 -0300

    Criado modelo de matrícula, vinculado alunos em cursos e adicionado recursos de matrícula na API

--- escola/models.py
+++ escola/models.py
@@ -22,3 +22,13 @@ class Curso(models.Model):
 
     def __str__(self):
         return self.descricao
+    
+class Matricula(models.Model):
+    periodo = (
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno')
+    )
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    periodo = models.CharField(max_length=1, choices=periodo, blank=False, null=False, default='M')
